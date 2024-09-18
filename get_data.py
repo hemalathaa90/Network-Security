c4307b10cdc074c081a037f9fd8cb7b024be6295
Hemalathaa <hemalathaaelumalai@gmail.com>
2024-09-18 21:32:12 +0200

    get_data code is updated

--- get_data.py
+++ get_data.py
@@ -8,5 +8,31 @@ from dotenv import load_dotenv
 import certifi
 import pandas as pd
 import numpy as np
+import pymongo
 
-from 
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+class NetworkDataExtact():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_tojson_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def pushing_data_to_mongodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    if __name__=='__main__':
+        pass
+    
