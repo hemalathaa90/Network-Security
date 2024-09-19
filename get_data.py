0958ada365d18dd8b332a553f0ca10a0dabfeecd
Hemalathaa <hemalathaaelumalai@gmail.com>
2024-09-19 21:46:29 +0200

    get_data updated

--- get_data.py
+++ get_data.py
@@ -25,18 +25,37 @@ class NetworkDataExtact():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def csv_tojson_converter(self):
+    def csv_tojson_converter(self,file_path):
         try:
-            pass
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self,records,database,collection):
         try:
-            pass
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    if __name__=='__main__':
-        pass
+if __name__=='__main__':
+    FILE_PATH="./Network_Data/NetworkData.csv"
+    DATABASE="Hema_Handson_Project"
+    COLLECTION="NetworkSecurity_EmailPhishingProject"
+    networkobj=NetworkDataExtact()
+    records=networkobj.csv_tojson_converter(FILE_PATH)
+    noofrecords=networkobj.pushing_data_to_mongodb(records,DATABASE,COLLECTION)
+    print(noofrecords)
 
