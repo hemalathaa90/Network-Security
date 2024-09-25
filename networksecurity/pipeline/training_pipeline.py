8c401e5cd6f591533d1b7466efaff2875ee62998
Hemalathaa <hemalathaaelumalai@gmail.com>
2024-09-25 20:46:33 +0200

    utils code updated

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -74,9 +74,14 @@ class TrainingPipeline:
             raise NetworkSecurityException(e,sys)'''
         
 
-    def start_data_transformation(self):
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
-            pass
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config)
+            
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
@@ -102,11 +107,14 @@ class TrainingPipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            print(f"Data Ingestion Artifact: {data_ingestion_artifact}")
+            #print(f"Data Ingestion Artifact: {data_ingestion_artifact}")
         
             # Start data validation
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            print(f"Data Validation Artifact: {data_validation_artifact}")
+            #print(f"Data Validation Artifact: {data_validation_artifact}")
+            #start data transformation
+            data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            print(data_transformation_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
